2d4b8e326bcae430b2c0bc3ba44b079f72f24735
ckatprakash <ckatprakash@outlook.com>
2025-08-22 06:55:07 -0400

    Added new file

--- .github/workflows/main.tf
+++ .github/workflows/main.tf
@@ -1,20 +1,40 @@
-provider aws {
-    region = "us-west-2"
-    }
+provider "aws" {
+  region = var.aws_region
+}
+
+variable "aws_region" {
+  default = "us-west-2"
+}
+
+# Create VPC
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0/16"
-}
-resource aws_subnet "main" {
-    vpc_id = aws_vpc.main.id
-    cidr_block = "10.0.1/24"
-    availability_zone = "us-west-1a"
-    map_public_ip_on_launch = true
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "main-vpc"
+  }
 }
 
+# Create Subnet
+resource "aws_subnet" "main" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-west-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "main-subnet"
+  }
+}
+
+# Create Internet Gateway
 resource "aws_internet_gateway" "main" {
-    vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.main.id
+  tags = {
+    Name = "main-igw"
+  }
 }
 
+# Create Security Group
 resource "aws_security_group" "vpce_sg" {
   name        = "vpce-sg"
   description = "Security group for VPC Endpoint"
@@ -33,11 +53,32 @@ resource "aws_security_group" "vpce_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "vpce-sg"
+  }
 }
 
+# Get Default Route Table
+data "aws_route_table" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [aws_vpc.main.id]
+  }
+  filter {
+    name   = "association.main"
+    values = ["true"]
+  }
+}
+
+# Create VPC Endpoint for S3
 resource "aws_vpc_endpoint" "s3" {
   vpc_id            = aws_vpc.main.id
   service_name      = "com.amazonaws.${var.aws_region}.s3"
   vpc_endpoint_type = "Gateway"
-  route_table_ids   = [aws_vpc.main.default_route_table_id]
+  route_table_ids   = [data.aws_route_table.default.id]
+
+  tags = {
+    Name = "s3-endpoint"
+  }
 }
